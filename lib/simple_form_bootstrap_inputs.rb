94015c074c75326be1921ca4cc554ec652b61e42
ValentinAndreev <valentinandreev80@gmail.com>
2018-11-27 14:27:53 +0300

    final fix

--- lib/simple_form_bootstrap_inputs.rb
+++ lib/simple_form_bootstrap_inputs.rb
@@ -1,8 +1,8 @@
 require 'simple_form'
 require "simple_form_bootstrap_inputs/railtie"
+require "simple_form_bootstrap_inputs/currency_input"
+require "simple_form_bootstrap_inputs/date_picker_input"
+require "simple_form_bootstrap_inputs/datetime_picker_input"
 
 module SimpleFormBootstrapInputs
-  include "simple_form_bootstrap_inputs/currency_input"
-  include "simple_form_bootstrap_inputs/date_picker_input"
-  include "simple_form_bootstrap_inputs/datetime_picker_input"
 end
